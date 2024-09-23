25663718fb8696caea609999d192edfa68986d02
lukaGOA <lukachachiashvili31@gmail.com>
2024-09-23 20:07:55 +0400

    fifth commit

---
+++ LEVEL 40/HOMEWORK/level40.py
@@ -0,0 +1,19 @@
+# 1
+def double_integer(num):
+    return num * 2
+
+#2
+def greet(name):
+    return f"Hello, {name}!"
+
+#3
+def is_even(num):
+    return num % 2 == 0
+
+#4
+def greet_with_default(name="Guest"):
+    return f"Hello, {name}!"
+
+#5
+def nickname(name):
+    return name[:3]
