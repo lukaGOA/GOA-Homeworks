7a05a110c06d09fa083a98b0e3789c92c249a7f1
LukaChachiashvili <lukachachiashvili31@gmail.com>
2024-12-21 23:55:08 +0400

    commit

---
+++ LEVEL 80/CLASSWORK/script.js
@@ -0,0 +1,23 @@
+// 1
+const name = "Luka";
+const surname = "Tchatchiashvili";
+const age = "14";
+
+const info = name + " " + surname + ", " + age + ". " ;
+
+console.log(info);
+alert(info);
+
+// 2
+let num1 = 8 
+let num2 = 4
+
+let sum = num1 + num2;
+let deduction = num1 - num2;
+let multiply = num1 * num2;
+let division = num1 / num2;
+
+console.log("sum:", sum);
+console.log("deduction:", deduction);
+console.log("multiply:", multiply);
+console.log("division:", division);
