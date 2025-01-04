4fe882d358803e1ff5b61bc1fcaa418a5a8e6555
LukaChachiashvili <lukachachiashvili31@gmail.com>
2025-01-04 15:45:12 +0400

    commit

---
+++ LEVEL 85/CLASSWORK/script.js
@@ -0,0 +1,17 @@
+let choice = Number(prompt('Enter your choice: '));
+switch(choice) {
+    case 1:
+        alert('Americano');
+        console.log('Americano');
+        break;
+    case 2:
+        alert('Espresso');
+        console.log('Espresso');
+        break;
+    case 3:
+        alert('Cappuccino');
+        console.log('Cappuccino');
+        break;
+    default:
+        console.log('Please Enter Valid Number: '); 
+};
