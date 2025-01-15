d62bc47f276ba6a84fb63726b2cc95761e6a0bf7
LukaChachiashvili <lukachachiashvili31@gmail.com>
2025-01-15 14:21:05 +0400

    commit

---
+++ LEVEL 080/HOMEWORK/script.js
@@ -0,0 +1,18 @@
+// 1
+let name = "Luka";
+let surname = "Tchatchiashvili";
+let age = 14;
+
+//  2
+const country = "Georgia";
+const city = "Tbilisi";
+const car = "Mercedes";
+
+// 3
+// let-ში ჩვენ შეგვიძლია შევცვალოთ ცვლადის მნიშვნელობა.
+
+// 4
+// const-ში ჩვენ არ შეგვიძლია შევცვალოთცვლადის მნიშვნელობა.
+
+// 6
+// ფოტოს ჩავგდებ
