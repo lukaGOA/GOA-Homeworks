f3314487c6eb6df15f525d787ad41967305ade23
LukaChachiashvili <lukachachiashvili31@gmail.com>
2025-01-15 13:48:15 +0400

    commit

---
+++ LEVEL 005/CLASSWORK/level5.py
@@ -0,0 +1,7 @@
+num1 = int(input("please enter your num1:"))
+num2 = int(input("please enter your num2:"))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
---
+++ LEVEL 011/CLASSWORK/level11.py
@@ -0,0 +1,5 @@
+
+age1 = (input("enter your number:"))
+age2 = (input("enter your number:"))
+
+print(age1 == 14 or age2 == 14)
